6ea2b779e8c3f21dcc3ea77fc850ac8746bb1766
Aaron DeRuvo <aaron.deruvo@gmail.com>
2019-01-04 12:00:38 -0800

    project euler questions 1 and 2

---
+++ index.js
@@ -0,0 +1,53 @@
+const assert = require('assert');
+
+// Multiples of 3 and 5
+function euler1(n) {
+    let x = 0;
+    let sum = 0;
+
+    while (x < n) {
+
+        if (x%3 === 0 || x%5 === 0) {
+            sum += x;
+        }
+        x++;
+    }
+
+    return sum;
+}
+
+assert.equal(euler1(10), 23)
+
+assert.equal(euler1(100),2318)
+
+assert.equal(euler1(1000),233168)
+
+console.info('Tests Passed - Euler1')
+
+function evenFib(max) {
+    let x = 1
+    let evens = 0
+
+    const fib = function(value, previous = 1) {
+        if (value >= max) return
+
+        const next = value + previous
+
+        if (next % 2 === 0) { evens += next }
+
+        return  fib(next, value)
+    }
+
+    fib(1)
+
+    return evens
+}
+
+assert.equal(evenFib(10), 10)
+assert.equal(evenFib(100), 188)
+assert.equal(evenFib(4000000), 4613732)
+
+
+console.info('Tests Passed - evenFib')
+
+console.info('🎉 All Tests Pass 🎉')
